d928d214fb05062b35e5ff45df150d8becbaef19
Nmb1171 <nahombadebo72@gmail.com>
2024-12-30 17:05:20 +0300

    Update password script

--- activities/management/commands/update_password.py
+++ activities/management/commands/update_password.py
@@ -2,16 +2,22 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
 class Command(BaseCommand):
-    help = 'Update a user password'
+    help = 'Update or create a user and set the password'
 
     def handle(self, *args, **kwargs):
         username = 'nahom_fitness'
+        email = 'nahom@example.com'
         new_password = 'CLASHROYALE123*'
 
         try:
-            user = User.objects.get(username=username)
+            user, created = User.objects.get_or_create(username=username, email=email)
             user.set_password(new_password)
+            user.is_active = True
             user.save()
-            self.stdout.write(self.style.SUCCESS(f"Password for {username} updated successfully!"))
-        except User.DoesNotExist:
-            self.stdout.write(self.style.ERROR(f"User {username} does not exist."))
+
+            if created:
+                self.stdout.write(self.style.SUCCESS(f"User {username} created successfully!"))
+            else:
+                self.stdout.write(self.style.SUCCESS(f"Password for {username} updated successfully!"))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"Error: {str(e)}"))
